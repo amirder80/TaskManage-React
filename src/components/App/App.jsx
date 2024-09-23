f4b92a5ca04cc9055bbe9c4a7649b83f4a250974
amir derakhshan <amirderakhshangot80@gmail.com>
2024-09-23 18:59:48 +0330

    Deploy React app to GitHub Pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,12 @@
 
 import "./app.css"
 import Layout from "../Layout/Layout.jsx";
+import { BrowserRouter } from 'react-router-dom';
 const App = () => {
     return (
-        <>
+        <BrowserRouter>
             <Layout/>
-        </>
+        </BrowserRouter>
     )
 }
 export default App
